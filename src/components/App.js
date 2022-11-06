fa4ca388670808abdf3df25026d535bde509956f
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-11-07 00:44:07 +0100

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID LK0q0XlWTO5gz4p3YZPAz9ViSIQ2YWCHnydRuJn3qYY',
+      },
+    });
   }
 
   render() {
