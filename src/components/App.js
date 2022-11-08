aaac106d99f34eeed028fab9370a6acabe701765
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-11-08 14:22:59 +0100

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,25 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] };
+
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID LK0q0XlWTO5gz4p3YZPAz9ViSIQ2YWCHnydRuJn3qYY',
       },
     });
+
+    console.log(this);
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
